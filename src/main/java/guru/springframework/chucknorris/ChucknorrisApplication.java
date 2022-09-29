23c067b5497db9fdad0c189061b251f651c5804f
Monika Jakopanec <mjakopanec7@gmail.com>
2022-09-29 13:57:54 +0200

    Custom banner

--- src/main/java/guru/springframework/chucknorris/ChucknorrisApplication.java
+++ src/main/java/guru/springframework/chucknorris/ChucknorrisApplication.java
@@ -1,9 +1,7 @@
 package guru.springframework.chucknorris;
 
-import guru.springframework.chucknorris.controllers.JokeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ChucknorrisApplication {
